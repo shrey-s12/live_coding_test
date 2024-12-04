5919c306a58ea3f99d4d89903254d304f6fc0ada
Shrey Singhal <shreynbd12@gmail.com>
2024-12-04 19:16:31 +0530

    Component Design

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import ImageCard from "./components/ImageCard";
 import image1 from "./assets/download.jpeg";
 import image2 from "./assets/top-section-bg.jpeg";
 import image3 from "./assets/tree-736885_1280.jpg";
-import image4 from "./assets/error_icon.png";
+// import image4 from "./assets/error_icon.png";
 
 const images = [
   { url: image1, ready: true, error: false },
   { url: image2, ready: false, error: true },
   { url: image3, ready: true, error: false },
-  { url: image4, ready: false, error: true },
+  { url: "", ready: true, error: false },
 ];
 
 function App() {
--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import './ImageCard.css'; // Import custom CSS
+import React from 'react'
+import errorIcon from '../assets/error_icon.png'
 
 const ImageCard = ({ name, count, images }) => {
+    const hasError = images.some((image) => image.error);
     return (
-        <div className="card-container">
-            <div className="p-4 flex justify-center">
-                {images.map((image, index) => (
-                    <div key={index} className="relative mb-4">
-                        <img
-                            src={image.url}
-                            alt={`Image ${index}`}
-                            className="card-image" /* Apply custom CSS class */
-                        />
+        <div className="bg-gray-500 min-h-screen flex justify-center pt-12">
+            <div className='flex justify-between items-center bg-slate-950 h-48 w-[80%] px-20'>
+                <div className='flex justify-between items-center'>
+                    <div className='grid grid-cols-2'>
+                        {images.map((image, index) => (
+                            <div key={index}>
+                                {image.error
+                                    ? <img src={errorIcon} alt="error" className='h-[38px] w-[38px] rounded-3xl object-cover border' />
+                                    : <img src={image.url} alt="" className='h-[38px] w-[38px] rounded-3xl object-cover border' />}
+                            </div>
+                        ))
+                        }
                     </div>
-                ))}
-            </div>
-            <div className="card-content">
-                <h1 className="card-title">{name}</h1>
-                <p className="card-count">{count}+ offline centers</p>
+                    <div className='ml-12'>
+                        <h1 className='text-2xl text-white'>{name}</h1>
+                        <p className='text-white'>{count}+ offline centers</p>
+                    </div>
+                </div>
+                {hasError && <img src={errorIcon} alt="error" className='h-[38px] w-[38px] rounded-3xl object-cover border' />}
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default ImageCard;
+export default ImageCard
